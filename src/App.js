b4df602efbc315a5e67e0f802ed46ab5bf1b28d6
Adam Leskis <leskis@gmail.com>
2020-05-19 22:14:45 +0100

    working sandbox for an extremely basic test function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Controlled as CodeMirror } from 'react-codemirror2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+
+const initialFunctionString = `
+// complete the function below so it returns twice the input
+const multiplyByTwo = FILL_ME_IN;
+multiplyByTwo(10);
+`
+
+const testTheFunction = code => eval(code) === 20
+
+class App extends Component {
+  state = {
+    id: '',
+    js: initialFunctionString,
+  }
+
+  runCode = () => {
+    let documentContents;
+    const { js } = this.state;
+
+    const iframe = this.refs.iframe;
+    const document = iframe.contentDocument;
+    try {
+      if(testTheFunction(js)) {
+        documentContents = 'CONGRATS!!!!';
+      } else {
+        documentContents = 'Nope, not yet...';
+      }
+    } catch (error) {
+      documentContents = 'cannot parse the code';
+    }
+
+       document.open();
+       document.write(documentContents);
+       document.close();
+  };
+
+    render() {
+      const { handleCodeUpdate, runCode } = this;
+
+      const { js } = this.state;
+      const codeMirrorOptions = {
+        theme: 'material',
+        lineNumbers: true,
+        scrollbarStyle: null,
+        lineWrapping: true,
+      };
+
+      return (
+         <div className="App">
+            <section className="playground">
+              <div className="code-editor js-code">
+                <div className="editor-header">Higher Order Functions</div>
+                <CodeMirror
+                  value={js}
+                  options={{
+                    mode: 'javascript',
+                    ...codeMirrorOptions,
+                  }}
+                  onBeforeChange={(editor, data, js) => {
+                    this.setState({ js });
+                  }}
+                />
+              </div>
+              <iframe title="result" className="iframe" ref="iframe" />
+            </section>
+            <section>
+              <button className="runCode" onClick={runCode}>Run the Code</button>
+            </section>
+          </div>
+        );
+      }
+    }
 
 export default App;
